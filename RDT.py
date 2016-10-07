b77dec755c9e0360276e56988b256073f1e8b5c0
Chris Thomas <faore@onhaven.net>
2016-10-06 20:35:05 -0600

    no message

--- RDT.py
+++ RDT.py
@@ -165,6 +165,35 @@ class RDT:
                 self.network.udt_send(ack.get_byte_S())
                 print("\tSent ACK" + repr(self.seq_num) + "\n")
                 self.swapSeq()
+                # Wait to see if there is a duplicate so it can be discarded.
+                i = 0
+                while True:
+                    # Keep adding to the buffer.
+                    byte_S = self.network.udt_receive()
+                    self.byte_buffer += byte_S
+
+                    if (len(self.byte_buffer) >= Packet.length_S_length):
+                        # extract length of packet
+                        length = int(self.byte_buffer[:Packet.length_S_length])
+                        i += 1
+                        if(i == 1000):
+                            break
+                        if len(self.byte_buffer) >= length:
+                            # create packet from buffer content
+                            if Packet.corrupt(self.byte_buffer[0:length]):
+                                print("\tReceived corrupt packet. Sending NAK.")
+                                self.byte_buffer = self.byte_buffer[length:]
+                                self.network.udt_send(Packet(self.oppSeq, 'NAK').get_byte_S())
+                            else:
+                                p2 = Packet.from_byte_S(self.byte_buffer[0:length])
+                                if p2.msg_S == p.msg_S:
+                                    #duplicate, discard, resend ack.
+                                    print("\tReceived duplicate. Resending ACK.")
+                                    self.byte_buffer = self.byte_buffer[length:]
+                                    self.network.udt_send(ack.get_byte_S())
+                                    i = 0
+                                else:
+                                    return ret_S
 
 
                 # if this was the last packet, will return on the next iteration
